ba1e02d9da227ef3dc942db66e32f50351806a06
Ben Cavins <ben.cavins@flatironschool.com>
2024-08-29 15:55:47 -0400

    do lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,59 @@
 class Song:
-    pass
+    count = 0
+    artists = []
+    genres = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.increment_song_count()
+        Song.add_artist(artist)
+        Song.add_genre(genre)
+        
+    
+    @classmethod
+    def increment_song_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_artist(cls, artist):
+        """Add artist to cls.artists if it doesn't already exist"""
+        if artist not in cls.artists:
+            cls.artists.append(artist)
+        cls.update_artist_count(artist)
+    
+    @classmethod
+    def add_genre(cls, genre):
+        if genre not in cls.genres:
+            cls.genres.append(genre)
+        Song.update_genre_count(genre)
+
+    @classmethod
+    def update_genre_count(cls, genre):
+        if genre not in cls.genre_count:
+            cls.genre_count[genre] = 1  # first instance of this genre
+        else:
+            cls.genre_count[genre] += 1  # increment the existing value
+    
+    @classmethod
+    def update_artist_count(cls, artist):
+        if artist not in cls.artist_count:
+            cls.artist_count[artist] = 1  # create new key/value pair
+        else:
+            cls.artist_count[artist] += 1  # increment the existing value
+
+
+s1 = Song('ironic', 'alanis morriset', 'pop')
+s1 = Song('ironic', 'alanis morriset', 'pop')
+s2 = Song('sk8r boi', 'avril lavigne', 'pop')
+
+
+print(Song.count)
+print(Song.artists)
+print(Song.artist_count)
+print(Song.genres)
+print(Song.genre_count)
---
+++ lib/song2.py
@@ -0,0 +1,41 @@
+class Song:
+    all = []
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.all.append(self)
+
+    @classmethod
+    def get_song_count(cls):
+        return len(cls.all)
+
+    @classmethod
+    def get_artists(cls):
+        artists = set()
+        for song in cls.all:
+            artists.add(song.artist)
+        return artists
+    
+    @classmethod
+    def get_artist_counts(cls):
+        result = {}
+        for song in cls.all:
+            if song.artist not in result:
+                result[song.artist] = 1
+            else:
+                result[song.artist] += 1
+        return result
+
+
+s1 = Song('ironic', 'alanis morriset', 'pop')
+s1 = Song('ironic', 'alanis morriset', 'pop')
+s2 = Song('sk8r boi', 'avril lavigne', 'pop')
+
+
+print(Song.get_song_count())
+print(Song.get_artists())
+print(Song.get_artist_counts())
+# print(Song.genres)
+# print(Song.genre_count)
